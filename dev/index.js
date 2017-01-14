76805c3efd139cbcb241173ce0494f98a2a42a0a
Greg Peden <greg.j.peden@gmail.com>
2017-01-13 21:09:10 -0700

    Getting things rolling

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue/dist/vue.js'
-import Component from '../src/index.vue'
+import ClickConfirmation from '../src/clickConfirmation.vue'
 
 new Vue({
   el: '#app',
-  components: { app: Component },
-})
+  components: { ClickConfirmation }
+});
