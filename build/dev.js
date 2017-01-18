0b02faef8c4456b34905795ad59aab5f034edc0d
Greg Peden <greg.j.peden@gmail.com>
2017-01-17 20:16:36 -0700

    Semantic change

--- build/dev.js
+++ build/dev.js
@@ -3,17 +3,17 @@ var webpack = require('webpack');
 var config = require('./base');
 
 module.exports = Object.assign({}, config, {
-    entry: './dev/index.js',
-    output: {
-      path: path.resolve(__dirname, '../dev'),
-      publicPath: '/dev/',
-      filename: 'bundle.js'
-    },
-    devServer: {
-      historyApiFallback: true,
-      noInfo: true
-    },
-    devtool: '#eval-source-map'
+  entry: './dev/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dev'),
+    publicPath: '/dev/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
 });
 
 if (process.env.NODE_ENV === 'production') {
